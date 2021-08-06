61cbbc5757800abfc34d1b52ea48c6f66041bafa
Tatev84 <tatevh84@gmail.com>
2021-08-06 21:23:21 +0400

    homework 15

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*  let j = {Name:"Krishna",Email: "XYZ", CN : 12345};
+
+let json=JSON.stringify(j)
+alert(json)  */
+
+/*  let j='{"Name":"Krishna","Email":"XYZ", "CN": "12345"}';
+let user=JSON.parse(j)
+console.log(user)   */
+
+
